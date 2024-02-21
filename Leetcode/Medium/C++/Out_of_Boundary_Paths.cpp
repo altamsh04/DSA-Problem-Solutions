bbe8ac7d0c529ef81525474aaf2a67940ff2d0f6
altamsh <bairagdaraltamsh@gmail.com>
2024-02-21 22:10:18 +0530

    C++/

---
+++ Leetcode/Medium/C++/Out_of_Boundary_Paths.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int memo[51][51][51];
+    int mod = 1000000007;
+
+    long dfsPath(int m, int n, int maxMove, int i, int j) {
+
+        if (i>=m || i<0 || j>=n || j<0) {
+            return 1;
+        }
+        if (maxMove <= 0) {
+            return 0;
+        }
+        if (memo[i][j][maxMove] != -1) {
+            return memo[i][j][maxMove];
+        }
+        long res = 0;
+
+        res += dfsPath(m, n, maxMove-1, i-1, j);
+        res += dfsPath(m, n, maxMove-1, i+1, j);
+        res += dfsPath(m, n, maxMove-1, i, j+1);
+        res += dfsPath(m, n, maxMove-1, i, j-1);
+
+        return memo[i][j][maxMove] = res % mod;
+    }
+
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        memset(memo, -1, sizeof(memo));
+        return dfsPath(m, n, maxMove, startRow, startColumn) % mod;
+    }   
+};
