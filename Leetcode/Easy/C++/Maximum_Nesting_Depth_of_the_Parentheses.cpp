f3cca007df9580c082100d186b672e9014f40179
Altamsh Bairagdar <84860267+altamsh04@users.noreply.github.com>
2024-04-04 23:06:51 +0530

    Create  Maximum_Nesting_Depth_of_the_Parentheses.cpp

---
+++ Leetcode/Easy/C++/Maximum_Nesting_Depth_of_the_Parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  int maxDepth(string s) {
+    int ans = 0, opened = 0;
+
+    for (const char c : s) {
+      if (c == '(') {
+        ans = max(ans, ++opened);
+      }
+      else if (c == ')') {
+        --opened;
+      }
+    }
+    return ans;
+  }
+};
