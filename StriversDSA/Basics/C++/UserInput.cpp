3750784acad2f1389684a75c122486ce7c12ea9e
altamsh <bairagdaraltamsh@gmail.com>
2024-07-06 23:54:24 +0530

    Added UserInput.cpp

---
+++ StriversDSA/Basics/C++/UserInput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout<<"Enter number a : ";
+    cin>>a;
+
+    cout<<"Enter number b : ";
+    cin>>b;
+
+    cout<<"Addition of a + b is : "<<a+b;
+
+    return 0;
+}
