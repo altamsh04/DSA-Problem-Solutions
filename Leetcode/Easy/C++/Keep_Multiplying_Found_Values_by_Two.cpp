d19c9ec0d0b76fca3e99bef65bb9f6ab6d2d7531
Altamsh Bairagdar <84860267+altamsh04@users.noreply.github.com>
2024-07-13 00:00:51 +0530

    Create Keep_Multiplying_Found_Values_by_Two.cpp

---
+++ Leetcode/Easy/C++/Keep_Multiplying_Found_Values_by_Two.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        unordered_set<int> s;
+        for (int num : nums) {
+            s.insert(num);
+        }
+        while (s.count(original)) {
+            original <<= 1;
+        }
+        
+        return original;
+    }
+};
