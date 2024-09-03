652e6311c3529c5bd8b38b73da6edc1c3c3a6bd6
Altamsh Bairagdar <84860267+altamsh04@users.noreply.github.com>
2024-09-03 21:48:07 +0530

    Create Sum_of_Digits_of_String_After_Convert.cpp

---
+++ Leetcode/Easy/C++/Sum_of_Digits_of_String_After_Convert.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string num = "";
+        for (auto ch : s) {
+            num += to_string(ch - 'a' + 1);
+        }
+        
+        while (k--) {
+            int sum = 0;
+            for (char ch : num) {
+                sum += ch - '0';
+            }
+            num = to_string(sum);
+        }
+
+        return stoi(num);
+    }
+};
