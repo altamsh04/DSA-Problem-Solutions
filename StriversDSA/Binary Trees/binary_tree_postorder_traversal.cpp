c98d45105f8f33cfbd93737c0ca172567f4df575
Altamsh Bairagdar <84860267+altamsh04@users.noreply.github.com>
2024-09-30 00:08:18 +0530

    Create binary_tree_postorder_traversal.cpp

---
+++ StriversDSA/Binary Trees/binary_tree_postorder_traversal.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    vector<int> ans;
+
+    void postOrder(TreeNode* root) {
+        if (root == NULL || root == nullptr) return;
+
+        postOrder(root->left);
+        postOrder(root->right);
+        ans.push_back(root->val);
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        postOrder(root);
+        return ans;
+    }
+};
